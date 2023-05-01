212f5374fa1e852555304322cf529d03dbc2fddb
leandro <leandro.meira.alfonso@hotmail.com>
2023-05-01 01:59:39 -0300

    segundo commit

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    const dados = [
+        {
+            "id": 1,
+            "nome": "João",
+            "idade": 25,
+            "email": ""
+        },
+        {
+            "id": 2,
+            "nome": "Maria",
+            "idade": 30,
+            "email": ""
+        },
+    ]
+  res.json(dados)
 })
 
 app.listen(process.env.PORT || 3000)
