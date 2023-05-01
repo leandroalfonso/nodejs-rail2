c58b9669e19cf0dc90d6880096d6f168d25c5ebe
leandro <leandro.meira.alfonso@hotmail.com>
2023-05-01 19:50:11 -0300

    commit 55

--- index.js
+++ index.js
@@ -3,40 +3,42 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 
+const dados = [
+    {
+      "id": 1,
+      "nome": "João",
+      "idade": 25,
+      "email": ""
+    },
+    {
+      "id": 2,
+      "nome": "Maria",
+      "idade": 30,
+      "email": ""
+    },
+    {
+      "id": 3,
+      "nome": "Pedro",
+      "idade": 28,
+      "email": "pedro@example.com"
+    },
+    {
+      "id": 4,
+      "nome": "Ana",
+      "idade": 35,
+      "email": "ana@example.com"
+    },
+    {
+      "id": 5,
+      "nome": "Lucas",
+      "idade": 22,
+      "email": "lucas@example.com"
+    }
+  ]
+
 app.get('/', function (req, res) {
-    const dados = [
-        {
-          "id": 1,
-          "nome": "João",
-          "idade": 25,
-          "email": ""
-        },
-        {
-          "id": 2,
-          "nome": "Maria",
-          "idade": 30,
-          "email": ""
-        },
-        {
-          "id": 3,
-          "nome": "Pedro",
-          "idade": 28,
-          "email": "pedro@example.com"
-        },
-        {
-          "id": 4,
-          "nome": "Ana",
-          "idade": 35,
-          "email": "ana@example.com"
-        },
-        {
-          "id": 5,
-          "nome": "Lucas",
-          "idade": 22,
-          "email": "lucas@example.com"
-        }
-      ]
-      
+    
+      console.log(dados)
   res.json(dados)
 })
 
